b24e0b29b10e25a6d4ecc32fd9380b9b7354a0d4
Liam Eller <liamthelastson@gmail.com>
2022-01-14 11:59:40 -0800

    WIP: add point system to languages to determine which is best for user

--- js/main.js
+++ js/main.js
@@ -1,18 +1,47 @@
+let languages = {
+  "Javascript": [1, 2, 3, 3],
+  "Python": [2, 3, 3, 2],
+  "C#": [1, 2, 2, 1]
+}
+
+function langComp(user, lang) {
+  let userScore = 0;
+
+  for (let i = 0; i < user.length; i++) {
+    if (user[i] === lang[i]) {
+      userScore += 1;
+    }
+  }
+  return userScore;
+}
+
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
     event.preventDefault();
-    const sign = $("#sign").val();
+    const userSign = $("#sign").val();
     const drink = parseInt($("input[name='drink']:checked").val());
     const food = parseInt($("input[name='food']:checked").val());
     const landscape = parseInt($("input[name='landscape']:checked").val());
     const software = parseInt($("input[name='software']:checked").val());
-    console.log(sign, drink, food, landscape, software)
+    console.log(userSign, drink, food, landscape, software)
+    
+    let userChoices = [drink, food, landscape, software];
+    let bestMatch = ["", 0];
 
-    if (sign.length < 5) {
-      console.log("their sign is less than 5 letters")
+    if (userSign === "" || userSign.length < 3) {
+      $("#output").text("Please input a real astrological sign");
+      return;
     }
-    else {
-      console.log("there sign is longer than 5 letters")
+    
+    for (let key in languages) {
+      let tempMatch = langComp(userChoices, languages[key]);
+      
+      if (tempMatch > bestMatch[1]) {
+        bestMatch = [key, tempMatch];
+      }
     }
+
+    $("#ouput").text("You most match with " + bestMatch + " !!!");
+    $(".language").show();
   });
 });
